1541794817526c50ce95b521706a212661bcec98
KomarMonster <bszynkaruk@o2.pl>
2024-06-09 14:42:51 +0200

    LAB2 zad3 zrobione

---
+++ LAB2/zad3.py
@@ -0,0 +1,25 @@
+import socket
+
+address = '127.0.0.1'
+port = 2900
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(5)
+
+try:
+    sock.connect((address, port))
+    while True:
+        message = input("Wpisz wiadomosc: ")
+        if message == '': break
+        sock.send(message.encode())
+        response = sock.recv(1024)
+        print(response.decode())
+
+except socket.timeout:
+    print('Blad: Timeout')
+
+except socket.error as e:
+    print(f'Blad : {e}')
+
+finally:
+    sock.close()
