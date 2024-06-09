094fae920bab01622cdc8999dd7b5298b43c91bd
KomarMonster <bszynkaruk@o2.pl>
2024-06-09 20:50:53 +0200

    LAB4 zad6 zrobione

---
+++ LAB4/zad6.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((HOST, PORT))
+    while True:
+        data, addr = s.recvfrom(1024)
+        print(f"Otrzymano dane: {data} od {addr}")
+        data = socket.gethostbyname(data.decode())
+        s.sendto(data.encode(), addr)
