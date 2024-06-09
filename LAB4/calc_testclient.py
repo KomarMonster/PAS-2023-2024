ab8ac542467da68198af7e9d9a29d23b5f472cfd
KomarMonster <bszynkaruk@o2.pl>
2024-06-09 20:50:21 +0200

    Dodałem klientów do testów

---
+++ LAB4/calc_testclient.py
@@ -0,0 +1,22 @@
+import socket
+
+host = '127.0.0.1'
+port = 65432
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    sock.connect((host, port))
+    while True:
+        message = input("Podaj pierwsza liczbe: ")
+        sock.send(message.encode())
+        message = input("Podaj operator: ")
+        sock.send(message.encode())
+        message = input("Podaj druga liczbe: ")
+        sock.send(message.encode())
+        data = sock.recv(1024)
+        print(data.decode())
+except socket.error as e:
+    print("Blad: ", e)
+finally:
+    sock.close()
